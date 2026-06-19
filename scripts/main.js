d575a68876df5727719b6a1774e422254f99e595
iKiray <exkiray@gmail.com>
2026-06-19 01:16:18 -0400

    step 1 - add, subtract, multiply and divide functions

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+//step 1 - functions add, subtract, multiply, divide
+function add(n1, n2) {
+    return n1 + n2;
+}
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+function divide(n1, n2) {
+    return n1 / n2;
+}
+
+console.log(add(3,4));
+console.log(subtract(10,5));
+console.log(multiply(5,6));
+console.log(divide(49,7));
