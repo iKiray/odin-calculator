1b501c2e2a899ba26a784bdcbb2a16a12efa6a84
iKiray <exkiray@gmail.com>
2026-06-19 22:43:13 -0400

    fix button ids and add event listeners to calculator buttons

--- scripts/main.js
+++ scripts/main.js
@@ -31,3 +31,19 @@ function operate(op, n1, n2) {
 }
 
 //step 5 - button functions
+const displayUpdate = document.querySelector("input");
+document.getElementById("1").addEventListener("click", () => displayUpdate.value += 1);
+document.getElementById("2").addEventListener("click", () => displayUpdate.value += 2);
+document.getElementById("3").addEventListener("click", () => displayUpdate.value += 3);
+document.getElementById("4").addEventListener("click", () => displayUpdate.value += 4);
+document.getElementById("5").addEventListener("click", () => displayUpdate.value += 5);
+document.getElementById("6").addEventListener("click", () => displayUpdate.value += 6);
+document.getElementById("7").addEventListener("click", () => displayUpdate.value += 7);
+document.getElementById("8").addEventListener("click", () => displayUpdate.value += 8);
+document.getElementById("9").addEventListener("click", () => displayUpdate.value += 9);
+document.getElementById("0").addEventListener("click", () => displayUpdate.value += 0);
+document.getElementById("add").addEventListener("click", () => displayUpdate.value += " + ");
+document.getElementById("subtract").addEventListener("click", () => displayUpdate.value += " - ");
+document.getElementById("multiply").addEventListener("click", () => displayUpdate.value += " x ");
+document.getElementById("divide").addEventListener("click", () => displayUpdate.value += " / ");
+document.getElementById("clear").addEventListener("click", () => displayUpdate.value = "");
