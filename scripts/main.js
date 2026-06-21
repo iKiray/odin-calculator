3b900fdc32cfa9b26982d9794d5916d95cc3f3e0
iKiray <exkiray@gmail.com>
2026-06-21 03:01:48 -0400

    step 6 - make the calculator work

--- scripts/main.js
+++ scripts/main.js
@@ -13,37 +13,63 @@ function divide(n1, n2) {
 }
 
 //step 2 - creating 3 variables
-let number1;
-let number2;
-let operator;
+let num1;
+let num2;
+let oprt;
 
 //step 3 - 'operate' function
 function operate(op, n1, n2) {
-    if (op === "+") {
+    if (op === "add") {
         return add(n1, n2);
-    } else if (op === "-") {
+    } else if (op === "subtract") {
         return subtract(n1, n2);
-    } else if (op === "*") {
+    } else if (op === "multiply") {
         return multiply(n1, n2);
-    } else if (op === "/") {
+    } else if (op === "divide") {
         return divide(n1, n2);
     }
 }
 
 //step 5 - button functions
-const displayUpdate = document.querySelector("input");
-document.getElementById("1").addEventListener("click", () => displayUpdate.value += 1);
-document.getElementById("2").addEventListener("click", () => displayUpdate.value += 2);
-document.getElementById("3").addEventListener("click", () => displayUpdate.value += 3);
-document.getElementById("4").addEventListener("click", () => displayUpdate.value += 4);
-document.getElementById("5").addEventListener("click", () => displayUpdate.value += 5);
-document.getElementById("6").addEventListener("click", () => displayUpdate.value += 6);
-document.getElementById("7").addEventListener("click", () => displayUpdate.value += 7);
-document.getElementById("8").addEventListener("click", () => displayUpdate.value += 8);
-document.getElementById("9").addEventListener("click", () => displayUpdate.value += 9);
-document.getElementById("0").addEventListener("click", () => displayUpdate.value += 0);
-document.getElementById("add").addEventListener("click", () => displayUpdate.value += " + ");
-document.getElementById("subtract").addEventListener("click", () => displayUpdate.value += " - ");
-document.getElementById("multiply").addEventListener("click", () => displayUpdate.value += " x ");
-document.getElementById("divide").addEventListener("click", () => displayUpdate.value += " / ");
-document.getElementById("clear").addEventListener("click", () => displayUpdate.value = "");
+const display = document.querySelector("input");
+document.getElementById("1").addEventListener("click", () => display.value += 1);
+document.getElementById("2").addEventListener("click", () => display.value += 2);
+document.getElementById("3").addEventListener("click", () => display.value += 3);
+document.getElementById("4").addEventListener("click", () => display.value += 4);
+document.getElementById("5").addEventListener("click", () => display.value += 5);
+document.getElementById("6").addEventListener("click", () => display.value += 6);
+document.getElementById("7").addEventListener("click", () => display.value += 7);
+document.getElementById("8").addEventListener("click", () => display.value += 8);
+document.getElementById("9").addEventListener("click", () => display.value += 9);
+document.getElementById("0").addEventListener("click", () => display.value += 0);
+document.getElementById("clear").addEventListener("click", () => {
+    display.value = "";
+    num1 = "";
+    num2 = "";
+    oprt = "";
+});
+//step 6 - make the operate function work properly
+document.getElementById("add").addEventListener("click", () => {
+    num1 = display.value;
+    oprt = "add"
+    display.value = "";
+});
+document.getElementById("subtract").addEventListener("click", () => {
+    num1 = display.value;
+    oprt = "subtract"
+    display.value = "";
+});
+document.getElementById("multiply").addEventListener("click", () => {
+    num1 = display.value;
+    oprt = "multiply"
+    display.value = "";
+});
+document.getElementById("divide").addEventListener("click", () => {
+    num1 = display.value;
+    oprt = "divide"
+    display.value = "";
+});
+document.getElementById("equal").addEventListener("click", () => {
+    num2 = display.value;
+    display.value = operate(oprt, +num1, +num2);
+});
